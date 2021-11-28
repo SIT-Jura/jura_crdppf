062ed2248f8882822ceabbd2ee387786303b0d0d
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2021-11-28 07:25:18 +0100

    Finalize Oereb Client configuration (#2)
    * Finalize Oereb Client configuration

--- jura_crdppf/__init__.py
+++ jura_crdppf/__init__.py
@@ -6,22 +6,23 @@ import os
 def main(_, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-
     config = Configurator(settings=settings)
+    # Read and update settings for oereb client
     settings = config.get_settings()
     route_prefix = os.environ.get('OEREB_ROUTE_PREFIX', 'oereb')
-
-    # Settings for oereb client
-    #with open(settings.get('oereb_client.cfg'), encoding='utf-8') as f:
-    #    settings.update({
-    #        'oereb_client': yaml.load(f.read()).get('oereb_client')
-    #    })
+    with open(settings.get('oereb_client.cfg'), encoding='utf-8') as f:
+        settings.update({
+            'oereb_client': yaml.load(f.read()).get('oereb_client')
+        })
 
     # Include pyramid_oereb back-end configuration
     config.include('pyramid_oereb', route_prefix='oereb')
 
     # Including oereb_client configuration
-    #config.include('oereb_client', route_prefix='oereb')
+    config.include('oereb_client', route_prefix='oereb')
+
+    # Including custom views configuration
+    config.include('jura_crdppf.views', route_prefix='oereb')
 
     config.scan()
     return config.make_wsgi_app()
