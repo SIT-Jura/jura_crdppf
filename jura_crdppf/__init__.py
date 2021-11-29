65c59c21dbd8d554fee228d8bbd4ce4a74060c40
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2021-11-29 08:25:46 +0100

    Repair personal instance setup

--- jura_crdppf/__init__.py
+++ jura_crdppf/__init__.py
@@ -1,12 +1,16 @@
 from pyramid.config import Configurator
 import c2cwsgiutils.pyramid
+import os
 import yaml
 
 
 def main(_, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    route_prefix = os.environ.get('OEREB_ROUTE_PREFIX', 'oereb')
+
     config = Configurator(settings=settings)
+
     # Read and update settings for oereb client
     settings = config.get_settings()
     with open(settings.get('oereb_client.cfg'), encoding='utf-8') as f:
@@ -15,13 +19,13 @@ def main(_, **settings):
         })
 
     # Include pyramid_oereb back-end configuration
-    config.include('pyramid_oereb', route_prefix='oereb')
+    config.include('pyramid_oereb', route_prefix=route_prefix)
 
     # Including oereb_client configuration
-    config.include('oereb_client', route_prefix='oereb')
+    config.include('oereb_client', route_prefix=route_prefix)
 
     # Including custom views configuration
-    config.include('jura_crdppf.views', route_prefix='oereb')
+    config.include('jura_crdppf.views', route_prefix=route_prefix)
 
     config.scan()
     return config.make_wsgi_app()
