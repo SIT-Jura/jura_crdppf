288a783192caac2e15bd931748e139ecf9b0cde7
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2022-03-21 10:30:22 +0100

    Fix parcel search as per current tsearch contents of Jura

--- client/jura_crdppf_client/views/search.py
+++ client/jura_crdppf_client/views/search.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+import logging
 import pkg_resources
 
 from concurrent.futures import as_completed
 from defusedxml.ElementTree import fromstring
 from mako.template import Template
 from requests_futures.sessions import FuturesSession
+
+log = logging.getLogger(__name__)
 
 
 def hook_egrid(config, response, lang, default_lang):
@@ -14,7 +17,11 @@ def hook_egrid(config, response, lang, default_lang):
     results = []
 
     for result in response.get('features'):
-        egrid = result.get('properties').get('label')[8:]
+        result_properties = result.get('properties')
+        result_label = result_properties.get('label')
+        log.info("hook_egrid checking feature with layername {}, label {}".format(result_properties.get('layer_name'), result_label))
+
+        egrid = result_label[8:]
         results.append({
             'label': egrid,
             'egrid': egrid
@@ -30,8 +37,11 @@ def hook_address(config, response, lang, default_lang):
     results = []
 
     for result in response.get('features'):
+        result_properties = result.get('properties')
+        result_label = result_properties.get('label')
+        log.info("hook_address checking feature with layername {}, label {}".format(result_properties.get('layer_name'), result_label))
         results.append({
-            'label': result.get('properties').get('label')[0:-10],
+            'label': result_label[0:-10],
             'coordinates': result.get('geometry').get('coordinates')
         })
 
@@ -43,20 +53,15 @@ def hook_real_estate(config, response, lang, default_lang):
     if len(features) < 1:
         return None
 
-    wfs_filter = Template(filename=pkg_resources.resource_filename('jura_crdppf_client.views', 'wfs_filter.xml'))
-
-    requests = []
-    headers = {'Content-Type': 'application/xml'}
-    session = FuturesSession(max_workers=len(features))
-    for result in features:
-        elements = result.get('properties').get('label')[0:-20].split(' ')
-        filter = wfs_filter.render(
-            layer_name='grundstueck',
-            parcel_number=elements.pop(),
-            municipality_name=' '.join(elements).replace(' (JU)', ''),
-            limit=1
-        ).encode('utf-8')
-        requests.append(session.post('https://geo.jura.ch', data=filter, headers=headers))
-
     results = []
 
+    for result in response.get('features'):
+        result_properties = result.get('properties')
+        result_label = result_properties.get('label')
+        log.info("hook_real_estate checking feature with layername {}, label {}".format(result_properties.get('layer_name'), result_label))
+        results.append({
+            'label': result_label,
+            'coordinates': result.get('geometry').get('coordinates')
+        })
+
+    return results
