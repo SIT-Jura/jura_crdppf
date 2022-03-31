035cbaec3fc3322cecab145fbd3c26bd03450c57
Wolfgang Kaltz <wolfgang.kaltz@camptocamp.com>
2022-03-31 14:01:44 +0200

    Add egrid reading in parcel search hook

--- client/jura_crdppf_client/views/search.py
+++ client/jura_crdppf_client/views/search.py
@@ -43,16 +43,6 @@ def hook_address(config, response, lang, default_lang):
     return results
 
 
-# Credits to https://progr.interplanety.org/en/python-how-to-find-the-polygon-center-coordinates/
-def centroid(vertexes):
-     _x_list = [vertex [0] for vertex in vertexes]
-     _y_list = [vertex [1] for vertex in vertexes]
-     _len = len(vertexes)
-     _x = sum(_x_list) / _len
-     _y = sum(_y_list) / _len
-     return(_x, _y)
-
-
 def hook_real_estate(config, response, lang, default_lang):
     features = response.get('features')
     if len(features) < 1:
@@ -63,15 +53,11 @@ def hook_real_estate(config, response, lang, default_lang):
     for result in response.get('features'):
         result_properties = result.get('properties')
         result_label = result_properties.get('label')
-        log.info("hook_real_estate reading feature with layername {}, label {}".format(result_properties.get('layer_name'), result_label))
-        parcel_geom = result.get('geometry').get('coordinates')
-        # Assumption: parcel geometry is always a multipolygon. Calculate the center of the first polygon of the center,
-        # this will be a point that is certainly in the parcel and can therefore be used for oereb getegrid call
-        parcel_first_polygon = parcel_geom[0][0]
-        parcel_first_center = centroid(parcel_first_polygon)
+        egrid_in_label = result_label[-15:-1]
+        log.info("hook_real_estate reading feature with layername {}, label {}, egrid_in_label {}".format(result_properties.get('layer_name'), result_label, egrid_in_label))
         results.append({
             'label': result_label,
-            'coordinates': parcel_first_center
+            'egrid': egrid_in_label
         })
 
     return results
